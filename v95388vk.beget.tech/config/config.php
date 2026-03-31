3a964c8bab45fa83e8193ce97cf76a36e7492de2
svetaorlova674-eng <svetaorlova674@gmail.com>
2026-03-31 22:58:57 +0700

    Delete v95388vk.beget.tech directory

--- v95388vk.beget.tech/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Статусы инвентаря
-define('STATUS_FREE',     'free');
-define('STATUS_BUSY',     'busy');
-define('STATUS_ARCHIVED', 'archived');
-
-$validStatuses = array(STATUS_FREE, STATUS_BUSY, STATUS_ARCHIVED);
